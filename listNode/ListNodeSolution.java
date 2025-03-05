99b4c4559232bc2e7d7265823e78ebc5d17ddcd9
bf <baofang1108@163.com>
2025-03-05 14:08:34 +0800

    feat: 链表 提交

--- listNode/ListNodeSolution.java
+++ listNode/ListNodeSolution.java
@@ -232,6 +232,40 @@ public class ListNodeSolution {
     }
 
     /**
+     * 142 - 给定一个链表的头节点  head ，返回链表开始入环的第一个节点。 如果链表无环，则返回 null。
+     *
+     * 如果链表中有某个节点，可以通过连续跟踪 next 指针再次到达，则链表中存在环。 为了表示给定链表中的环，评测系统内部使用整数 pos
+     * 来表示链表尾连接到链表中的位置（索引从 0 开始）。如果 pos 是 -1，则在该链表中没有环。注意：pos 不作为参数进行传递，仅仅是为了标识链表的实际情况。
+     *
+     * 解法：解法很简单，就是我们当做环形链表去处理，但是环形链表只判断了相交，当链表相交之后，我们从头开始 重新遍历，就可以找到环形起点了
+     *
+     * @param head
+     * @return
+     */
+    public ListNode detectCycle(ListNode head) {
+
+        ListNode fast = head;
+        ListNode slow = head;
+
+        while (fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+            if (slow == fast) {
+                // 链表已经相交了
+                slow = head;
+                while (slow != fast) {
+                    // 这时候都按照一步一步的走就行了
+                    slow = slow.next;
+                    fast = fast.next;
+                }
+                return slow;
+            }
+        }
+
+        return null;
+    }
+
+    /**
      * 相交链表
      *
      * - 给你两个单链表的头节点 headA 和 headB ，请你找出并返回两个单链表相交的起始节点。如果两个链表不存在相交节点，返回 null 。
